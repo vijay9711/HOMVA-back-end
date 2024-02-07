c2729bdba420ea89814699b18025547ad18b5ee3
Gumptionurge <113231578+Gumptionurge@users.noreply.github.com>
2024-02-07 12:53:43 -0600

    prop change

--- src/main/java/com/example/realEstate/controller/PropertyController.java
+++ src/main/java/com/example/realEstate/controller/PropertyController.java
@@ -13,7 +13,7 @@ import org.springframework.data.domain.Page;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PropertyController {
 
-    private final PropertyService propertyService;
+    private final PropertyService propertyService ;
 
     public PropertyController(PropertyService propertyService) {
         this.propertyService = propertyService;
